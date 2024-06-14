9b24daf940de07d72e31e3955f5f676fb8fdb516
Ashima Nair <ashima.nair01@gmail.com>
2024-06-14 09:54:19 -0500

    Initial Commit

---
+++ Game2/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by scholar on 6/12/24
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+    @StateObject var ticTacToeModel = TicTacToeModel()
+    @State var gameOver : Bool = false
+    
+        func buttonAction(_ index : Int) {
+            _ = self.ticTacToeModel.makeMove(index: index, player: .home)
+            self.gameOver = self.ticTacToeModel.gameOver.1
+        }
+        
+        var body: some View {
+            ZStack {
+                Color(red: 204/255, green: 227/255, blue: 222/255)
+                    .ignoresSafeArea()
+            
+                
+            VStack {
+                Spacer()
+                Text("Tic Tac Toe")
+                    .bold()
+                    .foregroundColor(Color.black)
+                    .padding(.bottom)
+                    .font(.title)
+                ForEach(0 ..< (ticTacToeModel.squares.count/3), content: {
+                    row in
+                    HStack {
+                        Spacer()
+                        ForEach(0 ..< 3, content: {
+                            column in
+                            let index = row * 3 + column
+                            SquareView(dataSource: ticTacToeModel.squares[index], action: {self.buttonAction(index)})
+                            Spacer()
+                        })
+                    }
+                    
+                })
+                Spacer()
+            }
+               
+            .alert(isPresented: self.$gameOver, content: {
+                Alert(title: Text("Game Over"),
+                      message: Text(self.ticTacToeModel.gameOver.0 != .empty ? self.ticTacToeModel.gameOver.0 == .home ? "You Win!": "AI Wins!" : "Nobody Wins" ) , dismissButton: Alert.Button.destructive(Text("Ok"), action: {
+                    self.ticTacToeModel.resetGame()
+                       }))
+            })
+              
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .frame(width:350, height: 400)
+                .cornerRadius(10)
+                .padding()
+                
+                    
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game2/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickityTacTowApp.swift
+//  Shared
+//
+//  Created by scholar on 6/12/24
+//
+
+import SwiftUI
+
+@main
+struct TickityTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
